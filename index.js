57d0b3ce53beffb82a27b98563d33b2641d259fa
camila <Camilasantosgbarboza091@gmail.com>
2023-04-13 19:42:23 -0300

    exerciciojs

---
+++ index.js
@@ -0,0 +1,6 @@
+alert ('Bem_vindo(a)');
+let nome = prompt('Digite seu nome:');
+let sobrenome = prompt('Digite seu sobrenome:');
+alert (`Nome completo: ${nome} ${sobrenome}`);
+alert(`Nome de catálogo: ${sobrenome.toUpperCase()}, ${nome}`);
+
